d9c40f059c15a331db2a51f36c235ab1b8903af0
艾鹏 <1987375309@qq.com>
2020-07-27 14:27:21 +0800

    单点登录功能

---
+++ route/route.php
@@ -0,0 +1,24 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006~2018 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+
+Route::get('think', function () {
+    return 'hello,ThinkPHP5!';
+});
+
+Route::get('hello/:name', 'index/hello');
+
+Route::domain('127.0.0.60', 'sso');
+Route::domain('127.0.0.61', 'pc');
+Route::domain('127.0.0.62', 'h5');
+
+return [
+
+];
---
+++ runtime/cache/sso/ef/0a37a2bd3c0422ac682ed555801acc.php
@@ -0,0 +1,4 @@
+<?php
+//000000000000
+ exit();?>
+i1jl0l62ffbdd0d8mvvffq2s44
